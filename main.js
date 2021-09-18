b0025987700e7afd9a8345ea5786c0135bf194d0
mansehajsingh <msehajs@gmail.com>
2021-09-17 23:00:57 -0600

    Modernizing appearance.

--- main.js
+++ main.js
@@ -26,3 +26,5 @@ app.use('/signup', signupRouter);
 app.listen(port, hostname, () => {
     console.log(`App is listening on Port #${port}.`);
 });
+
+// <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>

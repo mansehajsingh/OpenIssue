59fc8539a9b2b29be6505811fc583785fbe960c0
mansehajsingh <msehajs@gmail.com>
2022-03-29 22:46:37 -0600

    Added responsiveness to navbar component.

--- public/src/components/Navbar/Navbar.js
+++ public/src/components/Navbar/Navbar.js
@@ -1,106 +1,172 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { IconButton, others } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, ChevronLeftIcon } from "@chakra-ui/icons";
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+} from "@chakra-ui/react";
 
 import styles from "./styles.module.scss";
 import PropTypes from "prop-types";
 
 const whiteLogoPath = "../../../content/images/devwire_logo_white.svg";
 
-export const NavbarActiveItems =  {
+/* component-related constants */
+export const NavbarActiveItems = {
     Project: "project",
+    About: "about",
     Profile: "profile",
-    About: "about"
-}
+};
 
-const Navbar = ({
-    renderDrawer = null,
-    activeItem = null,
-    userID = null,
-}) => {
+/* component definition */
+const Navbar = ({ activeItem = null, userID = null }) => {
+    /* state */
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    // renders the drawer open button
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    /* effects */
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [windowWidth]);
+
+    /* render functions */
     const renderDrawerButton = () => {
-        if (!renderDrawer) return null;
-
-        let icon = (
-            <HamburgerIcon
-                color={"#FFF"}
-            />
-        );
+        let icon = <HamburgerIcon color={"#FFF"} />;
 
         return (
             <IconButton
                 icon={icon}
+                onClick={onOpen}
                 style={{
                     backgroundColor: "#202124",
-                    marginTop: "12px",
-                    marginRight: "30px",
+                    margin: "12px 40px 0 0",
                     border: "solid 1px #484F58",
                 }}
             />
         );
-    }
+    };
 
     const renderListItems = () => {
-
         let itemsElements = [];
 
-        const titleAndRouteByItem = { 
+        const titleAndRouteByItem = {
             Project: { title: "Projects", route: "/projects" },
-            Profile: { title: "Profile", route: userID ? `users/${userID}` : "/" },
+            Profile: {
+                title: "Profile",
+                route: userID ? `users/${userID}` : "/",
+            },
             About: { title: "About", route: "/about" },
-        }
+        };
 
         for (let item in NavbarActiveItems) {
-
-            itemsElements.push((
+            itemsElements.push(
                 <li>
-                    <Link 
+                    <Link
                         to={titleAndRouteByItem[item].route}
-                        className={ 
-                            activeItem === NavbarActiveItems[item] 
-                            ? styles.active_navlink : styles.inactive_navlink
+                        className={
+                            activeItem === NavbarActiveItems[item]
+                                ? styles.active_navlink
+                                : styles.inactive_navlink
                         }
                     >
                         {titleAndRouteByItem[item].title}
                     </Link>
                 </li>
-            ));
+            );
         }
-
         return itemsElements;
+    };
+
+    const renderDrawer = () => {
+        return (
+            <Drawer 
+                placement="right" 
+                onClose={onClose} 
+                isOpen={isOpen}
+                size="full"
+            >
+                <DrawerOverlay/>
+                <DrawerContent opacity={0.95}>
+                    <DrawerHeader 
+                        className={styles.drawer_header} 
+                        bgColor="#202124" 
+                        border="solid 0px"
+                    >
+                        <IconButton
+                            icon={<ChevronLeftIcon color="#FFF"/>}
+                            onClick={onClose}
+                            style={{
+                                backgroundColor: "#202124",
+                                margin: "0 40px 0 0",
+                                border: "solid 1px #484F58",
+                            }}
+                        />
+                        <img className={styles.drawer_logo} src={whiteLogoPath} /> 
+                    </DrawerHeader>
+                    <DrawerBody className={styles.drawer_body} border="solid 0px">
+                        <div className={styles.drawer_body_content}>
+                            <Link
+                                to={"/projects"}
+                                className={styles.drawer_link}
+                            >
+                                Projects
+                            </Link>
+                            <Link
+                                to={"/about"}
+                                className={styles.drawer_link}
+                            >
+                                About
+                            </Link>
+                            <Link
+                                to={`/about/${userID}`}
+                                className={styles.drawer_link}
+                            >
+                                Profile
+                            </Link>
+                        </div>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        );
     }
 
-     return (
+    return (
+        <>
         <nav className={styles.navbar}>
-            <div 
-                className={styles.navleft}
-                style={{ marginLeft: renderDrawer ? "60px" : "80px" }}
-            >
-                {renderDrawerButton()}
+            <div className={styles.navleft}>
                 <Link
                     to="/"
-                    onClick={(e) => window.location === "/" && e.preventDefault()}
+                    onClick={(e) =>
+                        window.location === "/" && e.preventDefault()
+                    }
                 >
-                    <img
-                        className={styles.navlogo}
-                        src={whiteLogoPath}
-                    />
+                    <img className={styles.navlogo} src={whiteLogoPath} />
                 </Link>
-                <ul className={styles.navlist}>
-                    {renderListItems()}
-                </ul>
+                {windowWidth > 500 && (
+                    <ul className={styles.navlist}>{renderListItems()}</ul>
+                )}
             </div>
+            <div>{windowWidth < 500 && renderDrawerButton()}</div>
         </nav>
+        {renderDrawer()}
+        </>
     );
 };
 
+/* component prop types */
 Navbar.propTypes = {
-    renderDrawer: PropTypes.func,
     activeItem: PropTypes.string,
     userID: PropTypes.string,
-}
+};
 
 export default Navbar;

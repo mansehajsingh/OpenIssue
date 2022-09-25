6941a0387115dbd5764910985c6f0b4f0b4e0801
mansehajsingh <msehajs@gmail.com>
2022-09-25 17:00:36 -0400

    Removed ca from https.createServer options.

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ process.on('uncaughtException', function (err) {
 server = https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/live/devwire.ca/key.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/devwire.ca/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/devwire.ca/chain.pem'),
+    //ca: fs.readFileSync('/etc/letsencrypt/live/devwire.ca/chain.pem'),
 }, app);
 
 server.listen(PORT, HOSTNAME, () => {
